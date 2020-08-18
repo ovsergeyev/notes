78e7820ae9a371fb2e478bb0e8fbe19f01eaa4c2
Sergeev Oleg <o.v.sergeyev@gmail.com>
2020-08-18 11:22:49 +0300

    Подключение vue-cli

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    
+    <!-- <header></header> -->
+    
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>Hello</h1>
+          <img alt="Vue logo" src="./assets/logo.png">
+        </div>
+      </section>
+    </div>
+    
+    <!-- <footer></footer> -->
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+</style>
